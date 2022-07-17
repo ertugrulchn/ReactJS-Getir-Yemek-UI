f9d4676b447d9809cdabd5899fd9c6a95f362f15
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-07-17 18:47:26 +0300

    Campaigns and favorites added

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,3 +1,24 @@
+import Title from "components/ui/Title";
+import Products from "api/products.json";
+import { useEffect, useState } from "react";
+import ProductItem from "components/ui/ProductItem";
+
 export default function Favorites() {
-  return <div>Favorites</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(Products);
+  }, []);
+
+  return (
+    <div>
+      <Title>Favoriler</Title>
+      <div className="grid grid-cols-8 gap-0.1 rounded overflow-hidden">
+        {products.length &&
+          products.map((product, index) => (
+            <ProductItem key={index} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 }
